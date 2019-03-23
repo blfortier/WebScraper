43e04e8c999edf202e1d3f93065312bafc2fa5e8
Brittney Fortier <brittfort34@gmail.com>
2019-03-22 23:53:36 -0400

    Created methods for some of the code in main. Created a table for the StockData database.

--- WebScraper/WebScraper/Program.cs
+++ WebScraper/WebScraper/Program.cs
@@ -5,37 +5,82 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace WebScraper
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             ChromeDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
 
-            driver.Navigate().GoToUrl("https://login.yahoo.com/");
-            driver.FindElement(By.Id("login-username")).SendKeys("webdriverproj" + Keys.Enter);
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.FindElement(By.Id("login-passwd")).SendKeys("Monkeys123" + Keys.Enter);
+            LogIn(driver);
 
             driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolios");
             driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
-                     
-            IList<IWebElement> stockData = driver.FindElements(By.ClassName("simpTblRow"));
-            IList<IWebElement> tableHeaders = driver.FindElements(By.XPath("//*[@id='pf - detail - table']/div[1]/table/thead/tr"));
-            Console.WriteLine("Total stocks: " + stockData.Count);
+            Scrape_DisplayStockData(driver);
 
-            //for(int i = 0; i < tableHeaders.Count; i++)
-            //{
-            //    Console.WriteLine(tableHeaders[i].Text);              
-            //}
+            ConnectToStockDataBase();       
+        }
+
+        public class StockRepository
+        {
+
+
+        }
+
+
+        public static void LogIn(ChromeDriver webScraper)
+        {
+            webScraper.Navigate().GoToUrl("https://login.yahoo.com/");
+            webScraper.FindElement(By.Id("login-username")).SendKeys("webdriverproj" + Keys.Enter);
+
+            webScraper.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            webScraper.FindElement(By.Id("login-passwd")).SendKeys("Monkeys123" + Keys.Enter);
+        }
+
+        public static void Scrape_DisplayStockData(ChromeDriver webScraper)
+        {
+            IList<IWebElement> stockData = webScraper.FindElements(By.ClassName("simpTblRow"));
+            IList<IWebElement> tableHeaders = webScraper.FindElements(By.TagName("th"));
+            Console.WriteLine("Total stocks: " + stockData.Count);
+            Console.WriteLine("Total headers: " + tableHeaders.Count);
+
+            //tableHeaders.RemoveAt(9);
+            //tableHeaders.RemoveAt(10);
+            //tableHeaders.RemoveAt(11);
+            //tableHeaders.RemoveAt(13);
+
+            Console.WriteLine("Total headers: " + tableHeaders.Count);
+
+            for (int i = 0; i < tableHeaders.Count; i++)
+            {
+                Console.Write(" {0}", tableHeaders[i].Text);
+            }
 
             for (int j = 0; j < stockData.Count; j++)
                 Console.WriteLine(stockData[j].Text);
+        }  
+        
+        public static void ConnectToStockDataBase()
+        {
+            string connectionString = null;
+            SqlConnection connect;
+            connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StockData;Integrated Security=True";
+            connect = new SqlConnection(connectionString);
 
-        }
+            try
+            {
+                connect.Open();
+                Console.WriteLine("Connection open!");
+                // connect.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot open connection...");
+            }
+        }        
     }
 }
---
+++ WebScraper/WebScraper/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WebScraper
+{
+    partial class Program
+    {
+        public class Stock
+        {
+            public int Id { get; set; }
+            public string Symbol { get; set; }
+            public float LastPrice { get; set; }
+            public float Change { get; set; }
+            public float ChangePercent { get; set; }
+            public string Currency { get; set; }
+            public DateTime MarketTime { get; set; }
+            public int Volume { get; set; }
+            public float AvgVol { get; set; }
+            public int MarketCap { get; set; }
+        }
+    }
+}
