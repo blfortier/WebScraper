63fbb39053e1cebd76b4b1ab7639dcfc3d916ff8
Brittney Fortier <brittfort34@gmail.com>
2019-04-17 23:14:57 -0400

    Created tables to hold the data pulled from Google Finance. Cerated a Stock class.

---
+++ ScraperUsingHAP/ScraperUsingHAP/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperUsingHAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stockInfo = new List<Stock>();
+
+            const string GoogleFinancePage = "https://www.google.com/finance";
+
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(GoogleFinancePage);
+
+            string stock;
+
+            if (htmlDoc.DocumentNode != null && htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Any())
+            {
+                
+            }
+                    // //*[@id="knowledge-finance-wholepage__financial-entities-list"]
+
+        }
+    }
+}
---
+++ ScraperUsingHAP/ScraperUsingHAP/Stock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperUsingHAP
+{
+    class Stock
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string LastPrice { get; set; }
+        public string Change { get; set; }
+
+        public Stock()
+        {
+
+        }
+
+        public Stock(string name, string symbol, string lastPrice, string change)
+        {
+            this.Name = name;
+            this.Symbol = symbol;
+            this.LastPrice = lastPrice;
+            this.Change = change;
+        }
+    }   
+}
