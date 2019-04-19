71076a332b74a24a43c756eac976e0a815df59c2
Brittney Fortier <brittfort34@gmail.com>
2019-04-19 05:32:28 -0400

    Created a HtmlWeb object.

--- ScraperUsingHAP/ScraperUsingHAP/Program.cs
+++ ScraperUsingHAP/ScraperUsingHAP/Program.cs
@@ -5,27 +5,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ScraperUsingHAP
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
             var stockInfo = new List<Stock>();
 
-            const string GoogleFinancePage = "https://www.google.com/finance";
+            string googleFinance = "https://www.google.com/finance#wptab=s:H4sIAAAAAAAAAOPQeMSozC3w8sc9YSmpSWtOXmMU4RJyy8xLzEtO9UnMS8nMSw9ITE_l2cXEHekfGhQfHOLv7B28iJU9DaIGAAUYQO1AAAAA";
 
-            var web = new HtmlWeb();
-            var htmlDoc = web.Load(GoogleFinancePage);
+            HtmlWeb web = new HtmlWeb();
+            var doc = web.Load(googleFinance);
 
-            string stock;
 
-            if (htmlDoc.DocumentNode != null && htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Any())
+            var root = doc.DocumentNode.SelectNodes("//*[@id='knowledge-finance-wholepage__financial-entities-list']/div");
+            //SelectNodes("//div[contains(@class,'ML43JB')]").ToList();
+
+            //foreach (var item in nameNode.Where(n => n.HasClass("TLh8uf")))
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //[@class='TLh8uf']");
+
+            Console.WriteLine(root);
+
+
+            if (root != null)
             {
-                
+                Console.WriteLine("yeah");
+                Console.WriteLine(root);
+                foreach (var item in root.Descendants())
+                {
+                    // Console.WriteLine(item.InnerText);
+                    // Console.WriteLine("in foreach loop");
+                    //var name = doc.DocumentNode.SelectSingleNode("/div[1]/g-link/a/div/span[1]").InnerText;
+                    Console.WriteLine(item);
+                }
             }
-                    // //*[@id="knowledge-finance-wholepage__financial-entities-list"]
+            else
+                Console.WriteLine("No");
+
+
+            // //*[@id='knowledge-finance-wholepage__financial-entities-list']/div
+
+            // "//*[contains(@class,'z4Fov')]")
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]/div[1]/g-link/a/div
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]
+            // #knowledge-finance-wholepage__financial-entities-list
+            // <span class="z4Fov">S&amp;P 500 Index</span>
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]/div[1]/g-link/a/div/span[1]
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]/div[1]/g-link/a/div/span[1]
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]/div[1]/g-link/a/div/span[1]
+            // //*[@id="knowledge-finance-wholepage__financial-entities-list"]
 
         }
+
     }
 }
