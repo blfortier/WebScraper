4548ebabe56e47bfab75138ee146b77ba80ae75d
Brittney Fortier <brittfort34@gmail.com>
2019-06-07 20:55:27 -0400

    Created a program to scrape stock data using IronWebScraper. I initialized the scraper and created a model for the stock data to be scraped.

---
+++ ScraperUsingIron/ScraperUsingIron/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperUsingIron
+{
+    public class Stock
+    {
+        private string _symbol;
+        private string _name;
+        private string _price;
+        private string _change;
+        private string _changePercent;
+        private string _volume;
+        private string _marketCap;
+
+        public string Symbol { get => _symbol; set => _symbol = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Price { get => _price; set => _price = value; }
+        public string Change { get => _change; set => _change = value; }
+        public string ChangePercent { get => _changePercent; set => _changePercent = value; }
+        public string Volume { get => _volume; set => _volume = value; }
+        public string MarketCap { get => _marketCap; set => _marketCap = value; }
+    }
+}
---
+++ ScraperUsingIron/ScraperUsingIron/StockScraper.cs
@@ -0,0 +1,34 @@
+﻿using IronWebScraper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperUsingIron
+{
+    public class StockScraper : WebScraper
+    {
+        public override void Init()
+        {
+            this.LoggingLevel = WebScraper.LogLevel.All;
+
+            HttpIdentity id = new HttpIdentity();
+            id.NetworkUsername = "webdriverproj";
+            id.NetworkPassword = "wdScraper135";
+            Identities.Add(id);
+            this.Request("https://www.msn.com/en-us/money/watchlist", Parse, id);
+        }
+
+        public override void Parse(Response response)
+        {
+            // //*[@id="watchlistsummarytab"]/tr
+            // #movie-featured > div
+            foreach (var Trs in response.Css("#watchlistsummarytab  > tr"))
+            {
+                var stock = new Stock();
+                //stock.Symbol = 
+            }
+        }
+    }
+}
