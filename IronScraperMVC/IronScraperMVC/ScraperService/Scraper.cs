4093a56bf2afd5001e6decb2ebe087fb83a57379
Brittney Fortier <brittfort34@gmail.com>
2019-07-03 22:18:43 -0400

    Added the files needed to perform a scrape using IronWebScraper.

--- IronScraperMVC/IronScraperMVC/ScraperService/Scraper.cs
+++ IronScraperMVC/IronScraperMVC/ScraperService/Scraper.cs
@@ -1,14 +1,13 @@
 ﻿using IronWebScraper;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace ScraperUsingIron
+namespace IronScraperMVC.ScraperService
 {
-    public class StockScraper : WebScraper
+    public class Scraper : WebScraper
     {
         List<Stock> listOfStocks = new List<Stock>();
 
@@ -22,36 +21,36 @@ namespace ScraperUsingIron
         {
             foreach (var row in response.Css("div.watchlist_dynamic1 > table > tbody > tr").Skip(1))
             {
-                 var stock = new Stock();
+                var stock = new Stock();
 
                 foreach (var name in row.Css("td.rowtitle > a"))
                 {
-                     stock.Name = ParseName(name);
+                    stock.Name = ParseName(name);
                 }
 
                 foreach (var symbol in row.Css("td.col_symbol"))
-	            {
+                {
                     stock.Symbol = symbol.InnerHtml;
-	            }
+                }
 
                 foreach (var price in row.Css("td.col_price"))
-	            {
+                {
                     stock.Price = price.InnerHtml;
-	            }
+                }
 
                 foreach (var change in row.Css("td.col_changecompound"))
-	            {
+                {
                     List<string> changeInfo = change.InnerText.Split().ToList();
 
                     stock.PriceChange = changeInfo[0];
                     stock.ChangePercent = changeInfo[1].Trim('(').Trim(')');
                     Console.WriteLine("cp: {0}", stock.ChangePercent);
-	            }
-                
+                }
+
                 foreach (var dollarVol in row.Css("td.col_dollarvolume"))
-	            {
+                {
                     stock.Volume = dollarVol.InnerText;
-	            }
+                }
 
                 Console.WriteLine();
 
@@ -83,4 +82,3 @@ namespace ScraperUsingIron
         }
     }
 }
-
